12119da6ccd800e3e9c935ada91e133b393a863a
Shunji Lin <lgxshunji@gmail.com>
2020-05-09 18:35:20 +0900

    add support for tailwindcss @apply inside styled-jsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,10 @@
-export default () => <h1 className="bg-teal-700">Hello world!</h1>;
+export default () => (
+  <>
+    <h1 className="text-lg">Hello world!</h1>
+    <style jsx>{`
+      h1 {
+        @apply bg-green-700;
+      }
+    `}</style>
+  </>
+);
